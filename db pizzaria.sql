bfeb3212bfa0fa210e7f727bc0e8a31dcee9d734
angelokapunda <angelokapunda@gmail.com>
2025-05-14 13:52:05 -0300

    :recycle: resoluçao do exercício

--- db pizzaria.sql
+++ db pizzaria.sql
@@ -36,18 +36,14 @@ INSERT INTO tb_pizzas (nome, valor, tamanho, categoria_id) VALUES
 
 SELECT nome FROM tb_pizzas WHERE valor > 45.00;
 
--- SELECT que retorna todas as pizzas cujo valor esteja no intervalo R$ 50,00 e R$ 100,00
 SELECT nome FROM tb_pizzas WHERE valor BETWEEN 50.00 AND 100.00;
 
--- SELECT utilizando o operador LIKE, buscando todas as pizzas que possuam a letra M no atributo nome
 SELECT nome FROM tb_pizzas WHERE nome LIKE '%M%';
 
--- SELECT utilizando a cláusula INNER JOIN, unindo os dados da tabela tb_pizzas com os dados da tabela tb_categorias
 SELECT tb_pizzas.nome, tb_categorias.nome AS categoria
 FROM tb_pizzas
 INNER JOIN tb_categorias ON tb_pizzas.categoria_id = tb_categorias.id;
 
--- SELECT utilizando a cláusula INNER JOIN, unindo os dados da tabela tb_pizzas com os dados da tabela tb_categorias, onde traga apenas as pizzas que pertençam a uma categoria específica (Exemplo: Todas as pizzas que são doce).
 SELECT tb_pizzas.nome, tb_categorias.nome AS categoria
 FROM tb_pizzas
 INNER JOIN tb_categorias ON tb_pizzas.categoria_id = tb_categorias.id
